ddb877de74d86584bdf75715411ba1670245a399
ArashAfsaryan <arashafsaryan@gmail.com>
2026-07-05 02:00:22 +0330

    Project cleanup and dead code removal

--- src/context/ToastContext.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-/* eslint-disable react-hooks/rules-of-hooks */
-const { showToast } = useToast();
-
-showToast({
-    type: "success",
-    title: "Settings Saved",
-    description: "Your changes have been saved."
-});
